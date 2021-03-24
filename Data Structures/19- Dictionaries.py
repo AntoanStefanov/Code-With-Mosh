b0b3a02d1afb6ffd9c56464962b5e9f686106a20
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-24 11:09:26 +0200

    Dictionaries

---
+++ Data Structures/19- Dictionaries.py
@@ -0,0 +1,4 @@
+# Powerful data structure - Dictionary
+# Collection of key: value pairs
+# We use it to map a key to a value
+# Real life example : phone book - we map person's name to their contact details.
