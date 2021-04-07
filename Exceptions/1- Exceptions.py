efee20811f712efa79e8fa30fb40907c5f8943a5
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-07 10:46:11 +0300

    Exceptions

--- Exceptions/1- Exceptions.py
+++ Exceptions/1- Exceptions.py
@@ -1,5 +1,5 @@
-numbers = [1, 2]
-print(numbers[2])
+# numbers = [1, 2]
+# print(numbers[2])
 
 # An exception is a kind of error that terminates the execution of a program.
 # This statement(line) threw an exception. - изключение
@@ -8,12 +8,12 @@ print(numbers[2])
 # That was thrown because of a programmer's mistake.
 
 
-# Another example: 
+# Another example:
 
 age = int(input('Age: '))
 
 # If user input is 'a' , program crashes
 # We get an exception of type value error.
 
-# It's your job to handle these exceptions and prevent 
+# It's your job to handle these exceptions and prevent
 # Your application from crashing
