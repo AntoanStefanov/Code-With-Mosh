b38ac9dfbd5038e1313b6d7ebae6f74b6feec76b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-18 14:39:00 +0300

    Cost Of Raising Exceptions

---
+++ Exceptions/7- Cost of Raising Exceptions.py
@@ -0,0 +1,7 @@
+# As I explained in the last lecture, when writing your own functions,
+# prefer not to raise exceptions, because those exceptions come with a price.
+# That's gonna show you in this lecture.
+
+# From the timeit module import function called timeit
+# with this function we can calculate the execution time of some code.
+
