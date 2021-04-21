dc48ac41c509336698465cdaa1ac7b2cc667b93b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-21 11:13:07 +0300

    Classes

---
+++ Classes/1- Classes.py
@@ -0,0 +1,42 @@
+# Defining a list of numbers
+numbers = [1, 2]
+# we learned that when we use the dot notation, we get access to all methods in list objects.
+# Every list object in Python has these methods.
+# numbers.
+
+# Wouldn't that be nice if we could create an object like shopping_cart and this object would have methods
+# like this:
+# shopping_cart.add()
+# shopping_cart.remove()
+# shopping_cart.get_total()
+
+# Another example:
+# Would be nice if we could have a point object with methods.
+# point.draw()
+# point.move()
+# point.get_distance() * between point x and point y
+
+
+# Here come classes
+# A class is a blueprint for creating new objects.
+# Throughout the course you have heard the term class.
+# For example: let's define a variable and set it to integer and print it's type
+
+x = 1
+print(type(x))
+# We see a class of int
+# So in Python we have a class called int for creating integers. Similarly, we have classes for creating booleans,
+# lists, dictionaries and so on. 
+# EVERY OBJECT WE HAVE IN PYTHON IS CREATED USING A CLASS WHICH IS A BLUEPRINT FOR CREATING OBJECTS OF THAT TYPE.
+# 
+# In this section you're going to learn how to create custom classes like customer, shopping_cart, point and so on.
+# Before that let's define a few terms.
+
+# 1.    Class: blueprint for creating new objects
+# 2.    Object: instance of a class
+
+# Example: 
+
+# Class: Human (this class will define all the attributes of humans) then we could create objects
+# Objects: John, Mary, ...
+
