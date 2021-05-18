e0c9faebc496fb0dd72c95c0cd6bc48c368e4b8d
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-18 21:38:36 +0300

    9 - Making Custom Containers

--- Classes/9- Making Custom Containers.py
+++ Classes/9- Making Custom Containers.py
@@ -29,6 +29,7 @@ class TagCloud:  # Creating a class
 
     # We can optionally add a method like add, that takes a tag
     def add(self, tag):
+        # If the key does not exist we get error so use get method
         # Here we should check if the tag has been added already. If we don't have it we are setting its value to 1,
         # otherwise increment the value with 1.
         # One way to implement this logic.
@@ -40,18 +41,39 @@ class TagCloud:  # Creating a class
 
         return self.tags.get(tag.lower(), 0)  # So if doesn't exist we return 0
 
+    # MY WAY ##
+        ####
+    # setattr(x, 'foobar', 123) is equivalent to x.foobar = 123.
+    # def assign_item(self, tag, value):
+    #     setattr(self, tag, value)
+        ####
 
-# # Because this class represents a container, it supports various operations around containers
-# cloud = TagCloud()  # creating an object
-# # Because this is a container we get get the number of items in this container,
-# len(cloud)
-# # We can get an item by it's key. For example: getting the number of articles tagged with python
-# cloud['python']
-# cloud['python'] = 10  # We can also set that
-# # And iterating over the container
-# for tag in cloud:
-#     print(tag)
-# # These are the operations that are supported by this custom container type
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        # using built in  functions to get iterator object .
+        # An interator object is an object that want a container and gets one item at a time.
+        # Call iter built in function. We want to iterate through self.tags
+        # RETURNS ITERATOR OBJECT  WHICH GIVES US ONE ITEM AT A TIME IN A FOR LOOP
+        return iter(self.tags)
+
+        # # Because this class represents a container, it supports various operations around containers
+        # cloud = TagCloud()  # creating an object
+        # # Because this is a container we get get the number of items in this container,
+        # len(cloud)
+        # # We can get an item by it's key. For example: getting the number of articles tagged with python
+        # cloud['python']
+        # cloud['python'] = 10  # We can also set that
+        # # And iterating over the container
+        # for tag in cloud:
+        #     print(tag)
+        # # These are the operations that are supported by this custom container type
+
+
 cloud = TagCloud()
 cloud.add('python')
 cloud.add('python')
@@ -85,4 +107,25 @@ print(cloud['js'])
 # Line 38 ^
 
 # As You can see with this implementation we can easily get the number of a given tag, we can't do this with
-# typical dict
+# typical dictionary. If we don't have the python tag our dictionary would throw an error
+
+# NEXT LEVEL
+
+# With the current implementation, you can only read the number of a given tag, you cannot set it like this
+# cloud.assign_item('C#', 10) LINE 43
+
+cloud['C#'] = 10
+print(cloud['C#'])
+# To do that we need to implement another magic method called __setitem__ (LINE 51)
+# With this implementation we can set the number of a given tag 105 line
+
+# Now in order to get the number of items in this tag cloud , we should implement the len magic method LINE 54
+print(cloud.__dict__)
+print(len(cloud))
+
+
+# And finally to make this iterable, so to iterate over a for loop.
+# We need to implement another magic method that is __iter__ LINE 58
+
+for tag in cloud:
+    print(tag)
