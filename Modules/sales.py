f6edb8d5673fec8929f1647c6b4b4f0bd8e02b35
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-08 08:42:43 +0300

    sales

---
+++ Modules/sales.py
@@ -0,0 +1,10 @@
+# This is our sales module, that file. 
+# All lowercase characters and underscore for seperation.
+# NAMING CONVETIONS.
+
+def calc_tax():
+    print('Calculating tax')
+
+
+def calc_shipping():
+    print('Calculating shipping')
